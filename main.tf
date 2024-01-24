018de1f75269cab1ede8761fe74ee2c4c018819a
David Cliff <david.cliff@datadivers.io>
2024-01-24 10:42:40 +0800

    Add table

--- main.tf
+++ main.tf
@@ -29,3 +29,31 @@ resource "snowflake_schema" "demo_schema" {
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
+
+resource "snowflake_table" "table" {
+  database            = "DEMO_DB"
+  schema              = "DEMO_SCHEMA"
+  name                = "table1"
+  comment             = "A table."
+
+  column {
+    name     = "id"
+    type     = "int"
+    nullable = true
+
+    default {
+      sequence = snowflake_sequence.sequence.fully_qualified_name
+    }
+  }
+
+  column {
+    name     = "identity"
+    type     = "NUMBER(38,0)"
+    nullable = true
+
+    identity {
+      start_num = 1
+      step_num  = 3
+    }
+  }
+}
